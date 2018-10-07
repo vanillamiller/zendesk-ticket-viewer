d2d4672a37ba070a654669150a4c31ace2fa1dd9
Anthony <agmiller794@gmail.com>
2018-10-07 14:28:44 +1100

    25 per page URL param

--- models/allTickets.js
+++ models/allTickets.js
@@ -1,9 +1,10 @@
 const axios = require("axios");
 
-module.exports.fetchAllTickets = axios.get("https://anthonymiller.zendesk.com/api/v2/tickets.json",
+module.exports.fetchAllTickets = axios.get("https://anthonymiller.zendesk.com/api/v2/tickets.json?per_page=25",
         {
             auth: {
                 username: 'agmiller794@gmail.com',
-                password: 'Evilboy4lif3!'
-            }
+                password: 'changeme'
+            },
+            
         });
