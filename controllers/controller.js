bf03c74f6fd7fd6e55356cb268bddab0059cdc53
Anthony <agmiller794@gmail.com>
2018-10-07 13:43:30 +1100

    total ticket count and status rendered

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,11 +3,10 @@ const fetchAllTickets = require("../models/allTickets").fetchAllTickets;
 let allTickets = function (req, res) {
 
     fetchAllTickets.then(response => {
-        tickets = response.data.tickets;
+        let tickets = response.data.tickets;
         console.log(tickets);
         res.render('index.ejs', { tickets: tickets });
-    })
-        .catch(errors => { res.render('index.ejs', { errors: errors }) });
+    }).catch(errors => { res.render('index.ejs', { errors: errors }) });
 }
 
 module.exports.allTickets = allTickets;
